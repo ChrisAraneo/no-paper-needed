e8552ccfdc4a446b906d2b9beb48dfe11e9f7e5b
Krzysztof Pająk <chris.araneo@gmail.com>
2024-12-15 01:22:44 +0100

    feat: remove boilerplate code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet],
+  imports: [RouterOutlet],
   standalone: true,
 })
 export class AppComponent {
