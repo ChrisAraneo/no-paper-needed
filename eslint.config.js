927a55c6ae14aad939c663df94046b9f4856ad01
Krzysztof Pająk <chris.araneo@gmail.com>
2025-02-20 21:54:15 +0100

    feat(eslint): update config, use import sort plugin, use jsonc plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,23 @@
-// @ts-check
+/* eslint-disable @typescript-eslint/no-require-imports */
+
 const eslint = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
+const jsonc = require('eslint-plugin-jsonc');
+const importSort = require('eslint-plugin-simple-import-sort');
 
 module.exports = tseslint.config(
   {
-    files: ['**/*.ts'],
+    files: ['src/**/*.ts', 'app.js', 'eslint.config.js'],
     extends: [
       eslint.configs.recommended,
       ...tseslint.configs.recommended,
       ...tseslint.configs.stylistic,
       ...angular.configs.tsRecommended,
     ],
+    plugins: {
+      'simple-import-sort': importSort,
+    },
     processor: angular.processInlineTemplates,
     rules: {
       '@angular-eslint/directive-selector': [
@@ -30,14 +36,32 @@ module.exports = tseslint.config(
           style: 'kebab-case',
         },
       ],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
     },
   },
   {
-    files: ['**/*.html'],
+    files: ['src/**/*.html'],
     extends: [
       ...angular.configs.templateRecommended,
       ...angular.configs.templateAccessibility,
     ],
     rules: {},
+  },
+  {
+    files: [
+      '.vscode/**/*.json',
+      'src/**/*.js',
+      '.prettierrc.json',
+      'angular.json',
+      'tsconfig.app.json',
+      'tsconfig.json',
+      'tsconfig.spec.json',
+    ],
+    extends: [...jsonc.configs['flat/recommended-with-json']],
+    rules: {
+      'jsonc/no-comments': 'error',
+      'jsonc/sort-keys': 'error',
+    },
   },
 );
