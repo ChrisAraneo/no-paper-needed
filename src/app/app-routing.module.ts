f2767071586835f773527160956bb7f438b1b71b
Krzysztof Pająk <chris.araneo@gmail.com>
2025-02-20 22:49:36 +0100

    style: format imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { NotesComponent } from './notes/notes.component';
 
 const routes: Routes = [
