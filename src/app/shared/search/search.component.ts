13ecba8ca9841c5da6c9bd41d4645f1e52ec52ff
Krzysztof Pająk <chris.araneo@gmail.com>
2025-06-22 16:45:58 +0200

    feat(search): implement searching

--- src/app/shared/search/search.component.ts
+++ src/app/shared/search/search.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+
+/* eslint-disable @angular-eslint/no-output-native */
 
 @Component({
   selector: 'npn-search',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.scss'
+  styleUrl: './search.component.scss',
 })
-export class SearchComponent {}
+export class SearchComponent {
+  @Output() readonly search = new EventEmitter<string>();
+
+  onSearch(element: HTMLInputElement, event: Event): void {
+    event.stopPropagation();
+    event.preventDefault();
+
+    this.search.emit(element.value.trim());
+    element.value = '';
+  }
+}
