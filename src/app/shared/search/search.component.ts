039774d77a42bd4a1956da9e4f974019b6aa82ce
Krzysztof Pająk <chris.araneo@gmail.com>
2025-06-22 14:28:33 +0200

    feat(search): create component

---
+++ src/app/shared/search/search.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'npn-search',
+  imports: [],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.scss'
+})
+export class SearchComponent {}
