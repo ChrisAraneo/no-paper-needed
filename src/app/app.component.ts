a64188a319a29161967b00eb03d06b90da89270e
Krzysztof Pająk <chris.araneo@gmail.com>
2026-01-06 04:52:23 +0100

    feat: use sidebar output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { Inject, LOCALE_ID } from '@angular/core';
+import { LOCALE_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 
@@ -7,6 +7,7 @@ import { ElectronService } from './core/services';
 import { AddNoteDialogComponent } from './shared/components/add-note-dialog/add-note-dialog.component';
 import { SearchbarComponent } from './shared/components/searchbar/searchbar.component';
 import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { Note } from './shared/interfaces/note.interface';
 
 @Component({
   selector: 'app-root',
@@ -26,6 +27,19 @@ export class AppComponent implements OnInit {
   private readonly translate = inject(TranslateService);
   private readonly locale = inject(LOCALE_ID);
 
+  protected openAddNoteDialog(): void {
+    this.isAddNoteDialogVisible = true;
+  }
+
+  protected closeAddNoteDialog(): void {
+    this.isAddNoteDialogVisible = false;
+  }
+
+  protected onSaveNote(note: Note): void {
+    console.log('Note saved:', note);
+    this.closeAddNoteDialog();
+  }
+
   ngOnInit(): void {
     const urlParams = new URLSearchParams(window.location.search);
     const locale = urlParams.get('locale');
@@ -43,9 +57,5 @@ export class AppComponent implements OnInit {
     } else {
       console.log('Run in browser');
     }
-
-    setTimeout(() => {
-      this.isAddNoteDialogVisible = true;
-    }, 2000);
   }
 }
