e448f0a4c84cac8ac62330977abca2f214153cc7
Krzysztof Pająk <chris.araneo@gmail.com>
2026-01-11 13:03:52 +0100

    style: format code

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -7,5 +7,5 @@ import { Component, Input } from '@angular/core';
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-    @Input() size: 'xl' | 'lg' = 'lg';
+  @Input() size: 'xl' | 'lg' = 'lg';
 }
