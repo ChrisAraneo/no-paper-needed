a90f147af05379cd88bf09e684f26777ef6d051b
Krzysztof Pająk <chris.araneo@gmail.com>
2026-01-11 12:21:20 +0100

    feat(home): use today component, use no notes translation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,11 @@
 import { AsyncPipe, CommonModule } from '@angular/common';
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
-import { format } from 'date-fns';
-import { interval, mergeMap, Subscription } from 'rxjs';
 
 import { StoreService } from '../core/services';
-import { HeaderComponent } from '../shared/components/header/header.component';
 import { NoteComponent } from '../shared/components/note/note.component';
+import { TodayComponent } from '../shared/components/today/today.component';
 import { LocaleService } from './../core/services/locale/locale.service';
-
-const MINUTE_MS = 60_000;
 
 @Component({
   selector: 'app-home',
@@ -18,43 +14,14 @@ const MINUTE_MS = 60_000;
     NoteComponent,
     CommonModule,
     AsyncPipe,
-    HeaderComponent,
+    TodayComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent {
   readonly localeService = inject(LocaleService);
   readonly storeService = inject(StoreService);
 
   protected notes = this.storeService.notes$;
-  protected now = '';
-
-  private readonly subscription = new Subscription();
-
-  ngOnInit(): void {
-    this.subscription.add(
-      this.localeService.get().subscribe((locale) => {
-        this.updateNow(locale);
-      }),
-    );
-
-    this.subscription.add(
-      interval(MINUTE_MS)
-        .pipe(mergeMap(() => this.localeService.get()))
-        .subscribe((locale) => {
-          this.updateNow(locale);
-        }),
-    );
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
-
-  private updateNow(locale: string): void {
-    this.now = format(new Date(), 'EEEE dd.MM', {
-      locale: this.localeService.getDateFnsLocale(locale),
-    }).replace(/^./u, (c) => c.toUpperCase());
-  }
 }
