c423e829e74ceb2cf5bb08b821de6f4ebfb68b3a
Krzysztof Pająk <chris.araneo@gmail.com>
2026-01-12 04:50:47 +0100

    feat(home): group notes by rows

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,15 @@
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { map } from 'rxjs';
 
 import { StoreService } from '../core/services';
 import { NoteComponent } from '../shared/components/note/note.component';
 import { TodayComponent } from '../shared/components/today/today.component';
 import { LocaleService } from './../core/services/locale/locale.service';
+
+const MAX_ROW_LENGTH = 3;
+const LAST_INDEX = -1;
 
 @Component({
   selector: 'app-home',
@@ -23,5 +27,16 @@ export class HomeComponent {
   readonly localeService = inject(LocaleService);
   readonly storeService = inject(StoreService);
 
-  protected notes = this.storeService.notes$;
+  protected notes = this.storeService.notes$.pipe(
+    map((notes) =>
+      notes.reduce<(typeof notes)[]>((rows, note, index) => {
+        if (index % MAX_ROW_LENGTH) {
+          rows.at(LAST_INDEX)?.push(note);
+        } else {
+          rows.push([note]);
+        }
+        return rows;
+      }, []),
+    ),
+  );
 }
