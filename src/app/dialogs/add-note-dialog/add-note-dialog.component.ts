99f61fe94037c221e52d870135d3f3a163c092e7
Krzysztof Pająk <chris.araneo@gmail.com>
2026-01-12 05:10:40 +0100

    refactor(add-note-dialog): use stepper dialog component

--- src/app/dialogs/add-note-dialog/add-note-dialog.component.ts
+++ src/app/dialogs/add-note-dialog/add-note-dialog.component.ts
@@ -2,6 +2,7 @@
 /* eslint-disable @typescript-eslint/no-magic-numbers */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @angular-eslint/no-output-native */
+
 import { JsonPipe, NgClass } from '@angular/common';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import {
@@ -14,14 +15,18 @@ import {
 import { TranslateModule } from '@ngx-translate/core';
 import { ButtonModule } from 'primeng/button';
 import { DatePickerModule } from 'primeng/datepicker';
-import { DialogModule } from 'primeng/dialog';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { InputTextModule } from 'primeng/inputtext';
 import { RadioButtonModule } from 'primeng/radiobutton';
-import { StepperModule } from 'primeng/stepper';
 import { TextareaModule } from 'primeng/textarea';
 
+import { StepAction } from '../../shared/components/step/step.interfaces';
+import { StepPanelDirective } from '../../shared/components/stepper/step-panel.directive';
+import {
+  StepConfig,
+  StepperDialogComponent,
+} from '../../shared/components/stepper/stepper-dialog.component';
 import { SubheaderComponent } from '../../shared/components/subheader/subheader.component';
 import { Note } from '../../shared/interfaces/note.interface';
 import { ReminderMode } from '../../shared/interfaces/reminder-mode.enum';
@@ -29,7 +34,6 @@ import { ReminderMode } from '../../shared/interfaces/reminder-mode.enum';
 @Component({
   selector: 'app-add-note-dialog',
   imports: [
-    DialogModule,
     InputTextModule,
     ButtonModule,
     TextareaModule,
@@ -39,11 +43,12 @@ import { ReminderMode } from '../../shared/interfaces/reminder-mode.enum';
     TranslateModule,
     DatePickerModule,
     InputNumberModule,
-    StepperModule,
     RadioButtonModule,
     JsonPipe,
     NgClass,
     SubheaderComponent,
+    StepperDialogComponent,
+    StepPanelDirective,
   ],
   templateUrl: './add-note-dialog.component.html',
   styleUrl: './add-note-dialog.component.scss',
@@ -58,6 +63,13 @@ export class AddNoteDialogComponent implements OnInit {
   protected readonly ReminderMode = ReminderMode;
   protected activeStep = 1;
 
+  protected stepConfigs: StepConfig[] = [
+    { value: 1, label: 'DIALOGS.ADD_NOTE.DATE' },
+    { value: 2, label: 'DIALOGS.ADD_NOTE.CONTENT' },
+    { value: 3, label: 'DIALOGS.ADD_NOTE.REMINDERS' },
+    { value: 4, label: 'DIALOGS.ADD_NOTE.SUMMARY' },
+  ];
+
   ngOnInit(): void {
     this.form = new FormGroup({
       date: new FormControl(new Date()),
@@ -65,6 +77,79 @@ export class AddNoteDialogComponent implements OnInit {
       reminderMode: new FormControl(ReminderMode.SameDay, Validators.required),
       reminderDaysBefore: new FormControl(0),
     });
+  }
+
+  getStep1Actions(): StepAction[] {
+    return [
+      {
+        label: 'Next',
+        icon: 'pi pi-arrow-right',
+        iconPos: 'right',
+        onClick: () => (this.activeStep = 2),
+      },
+    ];
+  }
+
+  getStep2Actions(): StepAction[] {
+    return [
+      {
+        label: 'Back',
+        severity: 'secondary',
+        icon: 'pi pi-arrow-left',
+        onClick: () => (this.activeStep = 1),
+      },
+      {
+        label: 'Next',
+        icon: 'pi pi-arrow-right',
+        iconPos: 'right',
+        disabled:
+          !this.form.get('content')?.value ||
+          this.form.get('content')?.value.trim() === '',
+        onClick: () => {
+          this.form.get('content')?.markAsTouched();
+          if (
+            this.form.get('content')?.value &&
+            this.form.get('content')?.value.trim() !== ''
+          ) {
+            this.activeStep = 3;
+          }
+        },
+      },
+    ];
+  }
+
+  getStep3Actions(): StepAction[] {
+    return [
+      {
+        label: 'Back',
+        severity: 'secondary',
+        icon: 'pi pi-arrow-left',
+        onClick: () => (this.activeStep = 2),
+      },
+      {
+        label: 'Next',
+        icon: 'pi pi-arrow-right',
+        iconPos: 'right',
+        onClick: () => (this.activeStep = 4),
+      },
+    ];
+  }
+
+  getStep4Actions(): StepAction[] {
+    return [
+      {
+        label: 'Back',
+        severity: 'secondary',
+        icon: 'pi pi-arrow-left',
+        onClick: () => (this.activeStep = 3),
+      },
+      {
+        label: 'Save',
+        icon: 'pi pi-check',
+        iconPos: 'right',
+        onClick: () => this.saveNote(),
+      },
+    ];
   }
 
   saveNote(): void {
