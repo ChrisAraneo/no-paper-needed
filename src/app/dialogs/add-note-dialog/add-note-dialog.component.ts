e76f85400133d544234578b64f0f0472945abdc5
Krzysztof Pająk <chris.araneo@gmail.com>
2026-01-13 04:50:02 +0100

    refactor(add-note-dialog): add types to form, change updating step actions, add translations to actions

--- src/app/dialogs/add-note-dialog/add-note-dialog.component.ts
+++ src/app/dialogs/add-note-dialog/add-note-dialog.component.ts
@@ -1,10 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/unbound-method */
 /* eslint-disable @typescript-eslint/no-magic-numbers */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 /* eslint-disable @angular-eslint/no-output-native */
 
 import { JsonPipe, NgClass } from '@angular/common';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -12,7 +20,8 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslatePipe } from '@ngx-translate/core';
+import { isString } from 'lodash';
 import { ButtonModule } from 'primeng/button';
 import { DatePickerModule } from 'primeng/datepicker';
 import { FloatLabelModule } from 'primeng/floatlabel';
@@ -20,6 +29,7 @@ import { InputNumberModule } from 'primeng/inputnumber';
 import { InputTextModule } from 'primeng/inputtext';
 import { RadioButtonModule } from 'primeng/radiobutton';
 import { TextareaModule } from 'primeng/textarea';
+import { Subscription } from 'rxjs';
 
 import { StepAction } from '../../shared/components/step/step.interfaces';
 import { StepPanelDirective } from '../../shared/components/stepper/step-panel.directive';
@@ -30,6 +40,13 @@ import {
 import { SubheaderComponent } from '../../shared/components/subheader/subheader.component';
 import { Note } from '../../shared/interfaces/note.interface';
 import { ReminderMode } from '../../shared/interfaces/reminder-mode.enum';
+
+interface AddNoteDialogFormGroup {
+  date: FormControl<Date>;
+  content: FormControl<string>;
+  reminderMode: FormControl<ReminderMode>;
+  reminderDaysBefore: FormControl<number>;
+}
 
 @Component({
   selector: 'app-add-note-dialog',
@@ -53,14 +70,16 @@ import { ReminderMode } from '../../shared/interfaces/reminder-mode.enum';
   templateUrl: './add-note-dialog.component.html',
   styleUrl: './add-note-dialog.component.scss',
 })
-export class AddNoteDialogComponent implements OnInit {
+export class AddNoteDialogComponent implements OnInit, OnDestroy {
   @Input() isVisible = false;
 
   @Output() readonly save = new EventEmitter<Note>();
   @Output() readonly close = new EventEmitter<void>();
 
-  protected form!: FormGroup;
-  protected readonly ReminderMode = ReminderMode;
+  protected readonly reminderMode = ReminderMode;
+
+  protected form: FormGroup<AddNoteDialogFormGroup>;
+  protected stepActionsMap: Record<number, StepAction[]> = {};
   protected activeStep = 1;
 
   protected stepConfigs: StepConfig[] = [
@@ -70,86 +89,38 @@ export class AddNoteDialogComponent implements OnInit {
     { value: 4, label: 'DIALOGS.ADD_NOTE.SUMMARY' },
   ];
 
-  ngOnInit(): void {
-    this.form = new FormGroup({
-      date: new FormControl(new Date()),
-      content: new FormControl('', Validators.required),
-      reminderMode: new FormControl(ReminderMode.SameDay, Validators.required),
-      reminderDaysBefore: new FormControl(0),
+  private readonly subscription = new Subscription();
+
+  constructor(private readonly translatePipe: TranslatePipe) {
+    this.form = new FormGroup<AddNoteDialogFormGroup>({
+      date: new FormControl<Date>(new Date(), { nonNullable: true }),
+      content: new FormControl<string>('', {
+        nonNullable: true,
+        validators: [Validators.required],
+      }),
+      reminderMode: new FormControl<ReminderMode>(ReminderMode.SameDay, {
+        nonNullable: true,
+        validators: [Validators.required],
+      }),
+      reminderDaysBefore: new FormControl<number>(0, {
+        nonNullable: true,
+        validators: [Validators.min(0)],
+      }),
     });
   }
 
-  getStep1Actions(): StepAction[] {
-    return [
-      {
-        label: 'Next',
-        icon: 'pi pi-arrow-right',
-        iconPos: 'right',
-        onClick: () => (this.activeStep = 2),
-      },
-    ];
+  ngOnInit(): void {
+    this.updateStepActionsMap();
+
+    this.subscription.add(
+      this.form.get('content')?.valueChanges.subscribe(() => {
+        this.updateStepActionsMap();
+      }),
+    );
   }
 
-  getStep2Actions(): StepAction[] {
-    return [
-      {
-        label: 'Back',
-        severity: 'secondary',
-        icon: 'pi pi-arrow-left',
-        onClick: () => (this.activeStep = 1),
-      },
-      {
-        label: 'Next',
-        icon: 'pi pi-arrow-right',
-        iconPos: 'right',
-        disabled:
-          !this.form.get('content')?.value ||
-          this.form.get('content')?.value.trim() === '',
-        onClick: () => {
-          this.form.get('content')?.markAsTouched();
-          if (
-            this.form.get('content')?.value &&
-            this.form.get('content')?.value.trim() !== ''
-          ) {
-            this.activeStep = 3;
-          }
-        },
-      },
-    ];
-  }
-
-  getStep3Actions(): StepAction[] {
-    return [
-      {
-        label: 'Back',
-        severity: 'secondary',
-        icon: 'pi pi-arrow-left',
-        onClick: () => (this.activeStep = 2),
-      },
-      {
-        label: 'Next',
-        icon: 'pi pi-arrow-right',
-        iconPos: 'right',
-        onClick: () => (this.activeStep = 4),
-      },
-    ];
-  }
-
-  getStep4Actions(): StepAction[] {
-    return [
-      {
-        label: 'Back',
-        severity: 'secondary',
-        icon: 'pi pi-arrow-left',
-        onClick: () => (this.activeStep = 3),
-      },
-      {
-        label: 'Save',
-        icon: 'pi pi-check',
-        iconPos: 'right',
-        onClick: () => this.saveNote(),
-      },
-    ];
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   saveNote(): void {
@@ -159,36 +130,23 @@ export class AddNoteDialogComponent implements OnInit {
     if (this.form.invalid) {
       return;
     }
-    const reminderMode =
-      this.form.get('reminderMode')?.value ?? ReminderMode.SameDay;
-    let reminderDaysBefore: number | undefined;
 
-    switch (reminderMode) {
-      case ReminderMode.SameDay: {
-        reminderDaysBefore = 0;
+    const content = this.form.get('content')?.value;
+    const date = this.form.get('date')?.value;
 
-        break;
-      }
-      case ReminderMode.DayBefore: {
-        reminderDaysBefore = 1;
+    if (this.isString(content) && this.isDate(date)) {
+      const reminderMode =
+        this.form.get('reminderMode')?.value ?? ReminderMode.SameDay;
+      const reminderDaysBefore = this.getReminderDaysBeforeValue(reminderMode);
 
-        break;
-      }
-      case ReminderMode.MultipleDaysBefore: {
-        reminderDaysBefore = this.form.get('reminderDaysBefore')?.value ?? 0;
+      this.save.emit({
+        content,
+        date,
+        reminderDaysBefore,
+      });
 
-        break;
-      }
-      // No default
+      this.closeDialog();
     }
-
-    this.save.emit({
-      content: this.form.get('content')?.value,
-      date: this.form.get('date')?.value,
-      reminderDaysBefore: reminderDaysBefore ?? 0,
-    });
-
-    this.closeDialog();
   }
 
   closeDialog(): void {
@@ -199,5 +157,127 @@ export class AddNoteDialogComponent implements OnInit {
     if (!visible) {
       this.closeDialog();
     }
+  }
+
+  resetDialog(): void {
+    this.form.reset();
+    this.activeStep = 1;
+  }
+
+  private getReminderDaysBeforeValue(reminderMode: ReminderMode): number {
+    switch (reminderMode) {
+      case ReminderMode.SameDay: {
+        return 0;
+      }
+      case ReminderMode.DayBefore: {
+        return 1;
+      }
+      case ReminderMode.MultipleDaysBefore: {
+        return this.form.get('reminderDaysBefore')?.value ?? 0;
+      }
+      default: {
+        return 0;
+      }
+    }
+  }
+
+  private markContentFormControlAsTouched(): void {
+    this.form.get('content')?.markAsTouched();
+  }
+
+  private updateStepActionsMap(): void {
+    this.stepActionsMap = {
+      1: this.getStep1Actions(),
+      2: this.getStep2Actions(this.form.get('content')?.value),
+      3: this.getStep3Actions(),
+      4: this.getStep4Actions(),
+    };
+  }
+
+  private getStep1Actions(): StepAction[] {
+    return [
+      {
+        label: this.translatePipe.transform('DIALOGS.ACTIONS.NEXT'),
+        icon: 'pi pi-arrow-right',
+        iconPos: 'right',
+        onClick: (): void => {
+          this.activeStep = 2;
+        },
+      },
+    ];
+  }
+
+  private getStep2Actions(contentValue: string | undefined): StepAction[] {
+    return [
+      {
+        label: this.translatePipe.transform('DIALOGS.ACTIONS.BACK'),
+        severity: 'secondary',
+        icon: 'pi pi-arrow-left',
+        onClick: () => (this.activeStep = 1),
+      },
+      {
+        label: this.translatePipe.transform('DIALOGS.ACTIONS.NEXT'),
+        icon: 'pi pi-arrow-right',
+        iconPos: 'right',
+        disabled: !contentValue || contentValue.trim() === '',
+        onClick: (): void => {
+          this.markContentFormControlAsTouched();
+
+          if (contentValue && contentValue.trim() !== '') {
+            this.activeStep = 3;
+          }
+        },
+      },
+    ];
+  }
+
+  private getStep3Actions(): StepAction[] {
+    return [
+      {
+        label: this.translatePipe.transform('DIALOGS.ACTIONS.BACK'),
+        severity: 'secondary',
+        icon: 'pi pi-arrow-left',
+        onClick: (): void => {
+          this.activeStep = 2;
+        },
+      },
+      {
+        label: this.translatePipe.transform('DIALOGS.ACTIONS.NEXT'),
+        icon: 'pi pi-arrow-right',
+        iconPos: 'right',
+        onClick: (): void => {
+          this.activeStep = 4;
+        },
+      },
+    ];
+  }
+
+  private getStep4Actions(): StepAction[] {
+    return [
+      {
+        label: this.translatePipe.transform('DIALOGS.ACTIONS.BACK'),
+        severity: 'secondary',
+        icon: 'pi pi-arrow-left',
+        onClick: (): void => {
+          this.activeStep = 3;
+        },
+      },
+      {
+        label: this.translatePipe.transform('DIALOGS.ACTIONS.SAVE'),
+        icon: 'pi pi-check',
+        iconPos: 'right',
+        onClick: (): void => {
+          this.saveNote();
+        },
+      },
+    ];
+  }
+
+  private isString(value: unknown): value is string {
+    return isString(value);
+  }
+
+  private isDate(value: unknown): value is Date {
+    return value instanceof Date;
   }
 }
