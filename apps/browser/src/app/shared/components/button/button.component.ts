586e76828d1c64305d854fad1a51cd4152a7f130
Krzysztof Pająk <chris.araneo@gmail.com>
2026-01-29 20:20:06 +0100

    feat(button): create component

---
+++ apps/browser/src/app/shared/components/button/button.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ButtonIconPosition, ButtonModule, ButtonSeverity } from 'primeng/button';
+
+@Component({
+  selector: 'app-button',
+  imports: [ButtonModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
+  host: {
+    '[class.accent]': 'isAccent',
+  },
+})
+export class ButtonComponent {
+  @Input() label = '';
+  @Input() icon = '';
+  @Input() iconPos: ButtonIconPosition = 'right';
+  @Input() isDisabled = false;
+  @Input() severity: ButtonSeverity = 'primary';
+  @Input() isRounded = false;
+  @Input() styleClass = '';
+  @Input() isAccent = false;
+
+  @Output() readonly clicked = new EventEmitter<void>();
+
+  onClick(): void {
+    this.clicked.emit();
+  }
+}
+
