a97e3b8909d68e61576f4f8e04018d8468b3ac5c
Krzysztof Pająk <chris.araneo@gmail.com>
2026-02-21 17:50:03 +0100

    style: delete unused component import

--- apps/browser/src/app/dialogs/edit-note-dialog/edit-note-dialog.component.ts
+++ apps/browser/src/app/dialogs/edit-note-dialog/edit-note-dialog.component.ts
@@ -29,7 +29,6 @@ import { NoteDialog } from '../note-dialog.directive';
     DatePickerModule,
     InputNumberModule,
     RadioButtonModule,
-    NgClass,
     SubheaderComponent,
     StepperDialogComponent,
     StepPanelDirective,
