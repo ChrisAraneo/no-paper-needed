99a566501326898365e50403c3a3d8d06ed1518d
Krzysztof Pająk <chris.araneo@gmail.com>
2026-02-21 17:48:38 +0100

    fix: update prefix and fix loading i18n assets

--- apps/browser/src/main.ts
+++ apps/browser/src/main.ts
@@ -28,7 +28,7 @@ bootstrapApplication(AppComponent, {
     provideHttpClient(withInterceptorsFromDi()),
     provideTranslateService({
       loader: provideTranslateHttpLoader({
-        prefix: './assets/i18n/',
+        prefix: './i18n/',
         suffix: '.json',
       }),
       fallbackLang: 'en',
