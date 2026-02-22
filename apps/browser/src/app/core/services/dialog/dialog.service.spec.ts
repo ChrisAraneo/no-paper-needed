b79f32803d50529abe1c4aeee547ea6f24df4329
Krzysztof Pająk <chris.araneo@gmail.com>
2026-02-22 16:56:26 +0100

    test(dialog-service): add unit tests

--- apps/browser/src/app/core/services/dialog/dialog.service.spec.ts
+++ apps/browser/src/app/core/services/dialog/dialog.service.spec.ts
@@ -1,12 +1,67 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DialogService } from './dialog.service';
+import { Note } from '../../../shared/interfaces/note.interface';
 
 describe('DialogService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: DialogService;
+
+  const mockNote: Note = {
+    id: '1',
+    date: new Date(),
+    content: 'Test note',
+    reminderDaysBefore: 0,
+  };
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(DialogService);
+  });
 
   it('should be created', () => {
-    const service: DialogService = TestBed.inject(DialogService);
     expect(service).toBeTruthy();
+  });
+
+  describe('add note dialog', () => {
+    it('should open add note dialog', () => {
+      service.openAddNoteDialog();
+
+      expect(service.isAddNoteDialogOpen()).toBe(true);
+    });
+
+    it('should close add note dialog', () => {
+      service.openAddNoteDialog();
+      service.closeAddNoteDialog();
+
+      expect(service.isAddNoteDialogOpen()).toBe(false);
+    });
+  });
+
+  describe('edit note dialog', () => {
+    it('should open edit note dialog', () => {
+      service.openEditNoteDialog(mockNote);
+
+      expect(service.isEditNoteDialogOpen()).toBe(true);
+    });
+
+    it('should set edited note when opening', () => {
+      service.openEditNoteDialog(mockNote);
+
+      expect(service.editedNote()).toEqual(mockNote);
+    });
+
+    it('should close edit note dialog', () => {
+      service.openEditNoteDialog(mockNote);
+      service.closeEditNoteDialog();
+
+      expect(service.isEditNoteDialogOpen()).toBe(false);
+    });
+
+    it('should clear edited note when closing', () => {
+      service.openEditNoteDialog(mockNote);
+      service.closeEditNoteDialog();
+
+      expect(service.editedNote()).toBeUndefined();
+    });
   });
 });
