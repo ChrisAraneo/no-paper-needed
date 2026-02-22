633bea040354d231f4a30b424190b673bf8743b3
Krzysztof Pająk <chris.araneo@gmail.com>
2026-02-22 16:50:07 +0100

    feat(dialog-service): implement opening and closing dialog functions

--- apps/browser/src/app/core/services/dialog/dialog.service.ts
+++ apps/browser/src/app/core/services/dialog/dialog.service.ts
@@ -1,6 +1,34 @@
-import { Injectable } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
+
+import { Note } from '../../../shared/interfaces/note.interface';
 
 @Injectable({
   providedIn: 'root',
 })
-export class DialogService {}
+export class DialogService {
+  private readonly isAddNoteDialogVisible = signal(false);
+  private readonly isEditNoteDialogVisible = signal(false);
+  private readonly editedNote = signal<Note | undefined>(undefined);
+
+  readonly isAnyDialogOpen = computed(
+    () => this.isAddNoteDialogVisible() || this.isEditNoteDialogVisible(),
+  );
+
+  openAddNoteDialog(): void {
+    this.isAddNoteDialogVisible.set(true);
+  }
+
+  closeAddNoteDialog(): void {
+    this.isAddNoteDialogVisible.set(false);
+  }
+
+  openEditNoteDialog(note: Note): void {
+    this.editedNote.set(note);
+    this.isEditNoteDialogVisible.set(true);
+  }
+
+  closeEditNoteDialog(): void {
+    this.isEditNoteDialogVisible.set(false);
+    this.editedNote.set(undefined);
+  }
+}
