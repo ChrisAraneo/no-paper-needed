c868c948240fedbdde5adf9bc682c900d932f97b
Krzysztof Pająk <chris.araneo@gmail.com>
2026-02-23 08:24:46 +0100

    feat(functions): create getDayDiff util function

---
+++ apps/browser/src/app/shared/functions/get-day-diff.function.ts
@@ -0,0 +1,13 @@
+const DAY_MS = 1000 * 60 * 60 * 24;
+
+const normalizeDate = (date: Date): Date => {
+  const result = new Date(date);
+  result.setHours(0, 0, 0, 0);
+
+  return result;
+};
+
+export const getDayDiff = (a: Date, b: Date): number =>
+  Math.floor(
+    (normalizeDate(a).valueOf() - normalizeDate(b).valueOf()) / DAY_MS,
+  );
