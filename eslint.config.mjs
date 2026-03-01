7f9b7096bca7d1bf06ca087e3118c4bbfb690fa1
Krzysztof Pająk <chris.araneo@gmail.com>
2026-03-01 21:32:00 +0100

    style: format code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,22 @@
 import { configBuilder } from '@chris.araneo/eslint-config';
 
 const JSONS = ['**/*.json'];
-const IGNORED = ['.nx/cache/', '.nx/workspace-data/', 'apps/', 'libs/', 'node_modules/', 'package.json', 'package-lock.json'];
+const IGNORED = [
+  '.nx/cache/',
+  '.nx/workspace-data/',
+  'apps/',
+  'libs/',
+  'node_modules/',
+  'package.json',
+  'package-lock.json',
+];
 
 // TODO Handle undefined sources
 // TODO Handle undefined jsons
 // TODO Add module nx boundaries rules
 
-export default configBuilder().addNxConfig({ sources: [] }).addJsonConfig({ jsons: JSONS }).addIgnored({ ignored: IGNORED }).build();
+export default configBuilder()
+  .addNxConfig({ sources: [] })
+  .addJsonConfig({ jsons: JSONS })
+  .addIgnored({ ignored: IGNORED })
+  .build();
