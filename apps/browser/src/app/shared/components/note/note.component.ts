463851141765cd57ee549834d26b9fd0cfeee701
Krzysztof Pająk <chris.araneo@gmail.com>
2026-03-01 21:57:26 +0100

    feat(note): display reminder days, add tooltips

--- apps/browser/src/app/shared/components/note/note.component.ts
+++ apps/browser/src/app/shared/components/note/note.component.ts
@@ -1,13 +1,20 @@
 import { DatePipe } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CardModule } from 'primeng/card';
-
+import { TooltipModule } from 'primeng/tooltip';
 import { ButtonComponent } from '../button/button.component';
 import { Note } from '../../interfaces/note.interface';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-note',
-  imports: [CardModule, DatePipe, ButtonComponent],
+  imports: [
+    CardModule,
+    DatePipe,
+    ButtonComponent,
+    TooltipModule,
+    TranslateModule,
+  ],
   templateUrl: './note.component.html',
   styleUrl: './note.component.scss',
 })
