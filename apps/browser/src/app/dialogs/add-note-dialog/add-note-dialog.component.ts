a607681773664cd1325c6eb1ebd408d2bbb32963
Krzysztof Pająk <chris.araneo@gmail.com>
2026-03-01 21:57:35 +0100

    style: remove unused import

--- apps/browser/src/app/dialogs/add-note-dialog/add-note-dialog.component.ts
+++ apps/browser/src/app/dialogs/add-note-dialog/add-note-dialog.component.ts
@@ -1,4 +1,3 @@
-import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
