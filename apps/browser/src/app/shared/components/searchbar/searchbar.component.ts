9439b7d7fba67d22c421d8e8baf9d80a20a2e522
Krzysztof Pająk <chris.araneo@gmail.com>
2026-03-02 21:18:19 +0100

    feat(searchbar): add value input, add search output

--- apps/browser/src/app/shared/components/searchbar/searchbar.component.ts
+++ apps/browser/src/app/shared/components/searchbar/searchbar.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-searchbar',
-  imports: [IconFieldModule, InputIconModule, InputTextModule],
+  imports: [IconFieldModule, InputIconModule, InputTextModule, FormsModule],
   templateUrl: './searchbar.component.html',
   styleUrl: './searchbar.component.scss',
 })
-export class SearchbarComponent {}
+export class SearchbarComponent implements OnChanges {
+  @Input() value = '';
+  @Output() readonly search = new EventEmitter<string>();
+
+  protected model = '';
+
+  ngOnChanges(): void {
+    this.model = this.value;
+  }
+}
