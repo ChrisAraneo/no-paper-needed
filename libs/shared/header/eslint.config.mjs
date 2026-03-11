8d23418ab48a755298eab0e4bfec687064bd7776
Krzysztof Pająk <chris.araneo@gmail.com>
2026-03-11 19:02:25 +0100

    style: do not use local variables and use literals

--- libs/shared/header/eslint.config.mjs
+++ libs/shared/header/eslint.config.mjs
@@ -1,19 +1,12 @@
 import { configBuilder } from '@chris.araneo/eslint-config';
 
-const PREFIX = 'npn';
-const SOURCES = [/^(?!.*\.spec\.ts$).*\.ts$/.toString()];
-const TESTS = ['**/*.spec.ts'];
-const TEMPLATES = ['**/*.html'];
-const JSONS = ['**/*.json'];
-const IGNORED = ['eslint.config.mjs', 'vite.config.mts', 'src/test-setup.ts'];
-
 export default configBuilder()
   .addAngularConfig({
-    prefix: PREFIX,
-    sources: SOURCES,
-    tests: TESTS,
-    templates: TEMPLATES,
-    jsons: JSONS,
-    ignored: IGNORED,
+    prefix: 'npn',
+    sources: [/^(?!.*\.spec\.ts$).*\.ts$/.toString()],
+    tests: ['**/*.spec.ts'],
+    templates: ['**/*.html'],
+    jsons: ['**/*.json'],
+    ignored: ['eslint.config.mjs', 'vite.config.mts', 'src/test-setup.ts'],
   })
   .build();
