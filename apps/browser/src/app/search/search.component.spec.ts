a9b638cb5ca15a79c2770014e0d915491ee4311e
Krzysztof Pająk <chris.araneo@gmail.com>
2026-03-14 19:39:51 +0100

    test(search): add unit tests

---
+++ apps/browser/src/app/search/search.component.spec.ts
@@ -0,0 +1,31 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { SearchComponent } from './search.component';
+
+describe('SearchComponent', () => {
+  let component: SearchComponent;
+  let fixture: ComponentFixture<SearchComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [],
+      imports: [SearchComponent, TranslateModule.forRoot()],
+      providers: [provideRouter([])],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(SearchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render header', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('npn-header')).toBeTruthy();
+  });
+});
