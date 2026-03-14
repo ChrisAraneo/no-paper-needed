c818ee60345e526ca18a8c3e111f4750a63e0e38
Krzysztof Pająk <chris.araneo@gmail.com>
2026-03-14 16:33:57 +0100

    feat(note): display localized formatted date

--- apps/browser/src/app/shared/components/note/note.component.ts
+++ apps/browser/src/app/shared/components/note/note.component.ts
@@ -1,28 +1,44 @@
-import { DatePipe } from '@angular/common';
-import { Component, computed, input, output } from '@angular/core';
+import { Component, computed, inject, input, output } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { CardModule } from 'primeng/card';
 import { TooltipModule } from 'primeng/tooltip';
 import { ButtonComponent } from '@no-paper-needed/shared/button';
 import { Note, Recurrence } from '../../interfaces/note.interface';
 import { TranslateModule } from '@ngx-translate/core';
+import { format } from 'date-fns';
+import { LocaleService } from '../../../core/services/locale/locale.service';
+import { WEEKDAY_DAY_MONTH_DATE_FORMAT } from '../../consts/consts';
 
 @Component({
   selector: 'app-note',
-  imports: [
-    CardModule,
-    DatePipe,
-    ButtonComponent,
-    TooltipModule,
-    TranslateModule,
-  ],
+  imports: [CardModule, ButtonComponent, TooltipModule, TranslateModule],
   templateUrl: './note.component.html',
   styleUrl: './note.component.scss',
 })
 export class NoteComponent {
+  private readonly localeService = inject(LocaleService);
+  private readonly locale = toSignal(this.localeService.get(), {
+    initialValue: this.localeService.getCurrentLang(),
+  });
+
   readonly note = input<Note | undefined>(undefined);
   readonly showEditButton = input(false);
 
   readonly edit = output<Note>();
+
+  readonly formattedDate = computed(() => {
+    const date = this.note()?.date;
+
+    if (!date) {
+      return '';
+    }
+
+    const locale = this.locale();
+
+    return format(date, WEEKDAY_DAY_MONTH_DATE_FORMAT, {
+      locale: this.localeService.getDateFnsLocale(locale),
+    }).replace(/^./u, (c) => c.toUpperCase());
+  });
 
   readonly recurrenceLabel = computed(() => {
     const recurrence = this.note()?.recurrence;
