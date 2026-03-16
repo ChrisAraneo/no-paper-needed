45268fbd55de836b7ab7f372774cbd07c3417833
Krzysztof Pająk <chris.araneo@gmail.com>
2026-03-16 22:15:04 +0100

    feat(button): update storybook preview config

--- libs/shared/button/.storybook/preview.ts
+++ libs/shared/button/.storybook/preview.ts
@@ -1,6 +1,14 @@
 import type { Preview } from '@storybook/angular';
+import { applicationConfig } from '@storybook/angular';
+import { providePrimeNG } from 'primeng/config';
+import { THEME } from '../../../../apps/browser/src/app/shared/styles/theme';
 
 const preview: Preview = {
+  decorators: [
+    applicationConfig({
+      providers: [providePrimeNG({ theme: THEME })],
+    }),
+  ],
   parameters: {
     controls: {
       matchers: {
