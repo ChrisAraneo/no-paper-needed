ff6211f0d64190a23e62209ca904f339267a91c2
Krzysztof Pająk <chris.araneo@gmail.com>
2026-03-16 22:27:56 +0100

    feat(header): add storybook config, add stories

---
+++ libs/shared/header/.storybook/main.ts
@@ -0,0 +1,12 @@
+import type { StorybookConfig } from '@storybook/angular';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.@(ts|mdx)'],
+  framework: {
+    name: '@storybook/angular',
+    options: {},
+  },
+  addons: [],
+};
+
+export default config;
