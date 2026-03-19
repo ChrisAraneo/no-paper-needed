2b7a1f5b9147b0bb36eeadc3850b729e864aef9c
Krzysztof Pająk <chris.araneo@gmail.com>
2026-03-19 23:46:52 +0100

    feat(import): add service to exports

--- apps/browser/src/app/core/services/index.ts
+++ apps/browser/src/app/core/services/index.ts
@@ -1,4 +1,5 @@
 export * from './dialog/dialog.service';
 export * from './electron/electron.service';
 export * from './export/export.service';
+export * from './import/import.service';
 export * from './store/store.service';
