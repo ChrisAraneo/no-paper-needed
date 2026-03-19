273c9b0f51c3c6c38e64e4669867d7345cf5c2d7
Krzysztof Pająk <chris.araneo@gmail.com>
2026-03-19 22:56:05 +0100

    feat(export): create export service and implement exportToFile

---
+++ apps/browser/src/app/core/services/export/export.service.ts
@@ -0,0 +1,37 @@
+import { inject, Injectable } from '@angular/core';
+import { tap, first } from 'rxjs';
+
+import { NoteRecord } from '@no-paper-needed/shared/interfaces';
+import { StoreService } from '../store/store.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ExportService {
+  private readonly storeService = inject(StoreService);
+
+  exportToFile(): void {
+    this.storeService
+      .exportData()
+      .pipe(
+        first(),
+        tap((records) => this.downloadJson(records)),
+      )
+      .subscribe();
+  }
+
+  private downloadJson(records: NoteRecord[]): void {
+    const json = JSON.stringify(records, null, 2);
+    const blob = new Blob([json], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+
+    const anchor = document.createElement('a');
+    anchor.href = url;
+    anchor.download = `no-paper-needed-export-${new Date().toISOString().slice(0, 10)}.json`;
+    anchor.click();
+
+    // TODO Removing url and anchor element after download?
+
+    URL.revokeObjectURL(url);
+  }
+}
--- apps/browser/src/app/core/services/index.ts
+++ apps/browser/src/app/core/services/index.ts
@@ -1,3 +1,4 @@
 export * from './dialog/dialog.service';
 export * from './electron/electron.service';
+export * from './export/export.service';
 export * from './store/store.service';
