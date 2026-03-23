83f6611e366445b79cc83c6a36e6648fcd72526f
Krzysztof Pająk <chris.araneo@gmail.com>
2026-03-23 22:49:12 +0100

    feat(text-e2e): create lib, add e2e tests

---
+++ libs/shared/text-e2e/playwright.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig, devices } from '@playwright/test';
+import { nxE2EPreset } from '@nx/playwright/preset';
+import { workspaceRoot } from '@nx/devkit';
+
+const baseURL = process.env['BASE_URL'] || 'http://localhost:6010';
+
+export default defineConfig({
+  ...nxE2EPreset(__filename, { testDir: './src' }),
+  use: {
+    baseURL,
+    trace: 'on-first-retry',
+  },
+  webServer: {
+    command: 'npx nx run text:storybook --port=6010',
+    url: 'http://localhost:6010',
+    reuseExistingServer: true,
+    cwd: workspaceRoot,
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
