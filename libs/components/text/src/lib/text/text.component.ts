a06358fc1166ad16d7e784f136156a7f8f7ef801
Krzysztof Pająk <chris.araneo@gmail.com>
2026-03-31 21:54:08 +0200

    chore(text): explicitly set standalone to true

--- libs/components/text/src/lib/text/text.component.ts
+++ libs/components/text/src/lib/text/text.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'npn-text',
+  standalone: true,
   templateUrl: './text.component.html',
   styleUrl: './text.component.scss',
 })
